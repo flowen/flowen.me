b060661598637618206b50f3e1547a6e6d2c4fb2
fLowen <lowen@flowen.nl>
2019-03-27 15:57:05 +0200

    check if not gatsby

--- 2019/v3/src/lib/polyfill-foreach.js
+++ 2019/v3/src/lib/polyfill-foreach.js
@@ -1,9 +1,11 @@
-if ('NodeList' in window && !NodeList.prototype.forEach) {
-  console.info('polyfill for IE11')
-  NodeList.prototype.forEach = function(callback, thisArg) {
-    thisArg = thisArg || window
-    for (var i = 0; i < this.length; i++) {
-      callback.call(thisArg, this[i], i, this)
+if (typeof window !== `undefined`) {
+  if ('NodeList' in window && !NodeList.prototype.forEach) {
+    console.info('polyfill for IE11')
+    NodeList.prototype.forEach = function(callback, thisArg) {
+      thisArg = thisArg || window
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this)
+      }
     }
   }
 }
