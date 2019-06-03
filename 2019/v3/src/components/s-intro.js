fc1cbbe10ec02b747a39835534403d3be957e5f6
fLowen <lowen@flowen.nl>
2019-06-03 16:27:07 +0200

    updated intro

--- 2019/v3/src/components/s-intro.js
+++ 2019/v3/src/components/s-intro.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import { TimelineLite } from 'gsap';
-import { ease } from '../utils/ease';
-import logger from '../utils/logger';
+import React, { useEffect } from 'react'
+import { TimelineLite } from 'gsap'
+import { ease } from '../utils/ease'
+import logger from '../utils/logger'
 
-import R from './characters/r';
-import O from './characters/o';
-import U from './characters/u';
-import H from './characters/h';
-import N from './characters/n';
-import F from './characters/f';
-import A from './characters/a';
+import R from './characters/r'
+import O from './characters/o'
+import U from './characters/u'
+import H from './characters/h'
+import N from './characters/n'
+import F from './characters/f'
+import A from './characters/a'
 
 if (typeof window !== `undefined`) {
   // eslint-disable-next-line
-  require('../lib/DrawSVGPlugin.js');
+  require('../lib/DrawSVGPlugin.js')
 }
 
 const Intro = () => {
@@ -22,17 +22,17 @@ const Intro = () => {
     onComplete: () =>
       logger({
         category: 'Intro section',
-        action: 'User viewed section Intro'
-      })
-  });
+        action: 'User viewed section Intro',
+      }),
+  })
 
-  const m = 1.5; // multiplier
-  const t1 = 0.75 * m;
-  const t2 = 1.75 * m;
+  const m = 1.5 // multiplier
+  const t1 = 0.75 * m
+  const t2 = 1.75 * m
 
   useEffect(() => {
-    document.querySelector('.rouhun').classList.remove('not-visible');
-    document.querySelector('.container').classList.remove('not-visible');
+    document.querySelector('.rouhun').classList.remove('not-visible')
+    document.querySelector('.container').classList.remove('not-visible')
 
     // prettier-ignore
     tl.add('startLabel', 0)
@@ -48,7 +48,7 @@ const Intro = () => {
       .to('.fan .js--n', t2, { drawSVG: '0% 100%', ease }, 'lowerCaseLabel+=.5')
       .to('.occupation', t2, { opacity: 1, ease }, 'lowerCaseLabel+=.75')
       .play();
-  }, []);
+  }, [])
 
   return (
     <section className="intro">
@@ -68,10 +68,10 @@ const Intro = () => {
           <N />
         </div>
 
-        <p className="occupation">Creative developer &amp; designer.</p>
+        <p className="occupation">Creative developer with great taste.</p>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Intro;
+export default Intro
