d9bcf080f63def7396ff235d89a5e16990d1a75f
Rou Hun <lowen.f@gmail.com>
2021-08-09 18:01:12 +0200

    update twitter handle

--- 2021/frontend/components/FindMe.vue
+++ 2021/frontend/components/FindMe.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="find-me">
     <a href="mailto:b34ff8ed-85b6-49ff-877f-c72d181b4396@flowen.anonaddy.me">e-mail</a>
-    <a href="https://twitter.com/flowen_nl">twitter</a>
+    <a href="https://twitter.com/flowen_af">twitter</a>
     <a href="https://instagram.com/flowen_af">instagram</a>
     <a href="https://read.cv/flowen">read.cv</a>
   </section>
